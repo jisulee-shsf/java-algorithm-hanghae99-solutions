c5ccf8dc4b605ada5af74e96144a3f05e9dfd266
Jisu Lee <jisulee.shsf@gmail.com>
2023-12-26 09:18:30 +0900

    Upload _022_Solution.java

---
+++ _022_Solution.java
@@ -0,0 +1,12 @@
+public class _022_Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        int min = Math.min(a, b);
+        int max = Math.max(a, b);
+
+        for (int i = min; i <= max; i++) {
+            answer += i;
+        }
+        return answer;
+    }
+}

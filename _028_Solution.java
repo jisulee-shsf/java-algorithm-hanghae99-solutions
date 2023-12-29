f2d028d5a299d671f1119c9aaf4a8b22c7bae3e3
Jisu Lee <jisulee.shsf@gmail.com>
2023-12-29 09:27:08 +0900

    Upload _028_Solution.java

---
+++ _028_Solution.java
@@ -0,0 +1,11 @@
+public class _028_Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return answer - sum;
+    }
+}
